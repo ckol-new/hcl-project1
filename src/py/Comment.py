a68f0161dee18f3552ecfe842fc9d2e0398f63d0
ckol <christopher.kollar@ucalgary.ca>
2026-01-03 16:19:23 -0700

    2026-01-03: serialization and equality methods of Comment.py is implemented

--- src/py/Comment.py
+++ src/py/Comment.py
@@ -1,3 +1,5 @@
+from Author import Author
+
 class Comment:
     def __init__(self, url: str, post_id: int, date: str,  author: Author, content: list[str]):
         self.url = url
@@ -5,3 +7,32 @@ class Comment:
         self.author = author
         self.date = date
         self.content = content
+
+    def to_dict(self) -> dict:
+        return {
+            'url': self.url,
+            'post_id': self.post_id,
+            'date': self.date,
+            'author': self.author.to_dict(),
+            'content': self.content
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+         return cls(
+             url=data['url'],
+             post_id=data['post_id'],
+             date=data['date'],
+             author=Author.from_dict(data['author']),
+             content=data['content']
+         )
+
+    def __eq__(self, other):
+        if not isinstance(other, Comment): return False
+        return (
+            self.url == other.url and
+            self.post_id == other.post_id and
+            self.date == other.date and
+            self.author == other.author and
+            self.content == other.content
+        )
