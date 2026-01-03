0a201eae16c705ac47a12fc296d5e1e8d9b0c723
ckol <christopher.kollar@ucalgary.ca>
2026-01-03 16:06:50 -0700

    2026-01-03: __init__ method of Author class

---
+++ src/py/Author.py
@@ -0,0 +1,5 @@
+class Author:
+    def __init__(self, user_name: str, user_id: int, profile_link: str):
+        self.user_name = user_name
+        self.user_id = user_id
+        self.profile_link = profile_link
