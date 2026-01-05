304185f864fc0f0eb1b8b7004cfecdf9d923db6d
ckol <christopher.kollar@ucalgary.ca>
2026-01-05 14:23:45 -0700

    2026-01-05:

--- src/py/tests/TestScrape.py
+++ src/py/tests/TestScrape.py
@@ -2,7 +2,6 @@ from py.model.ScrapingPipeline import ALZConnectedScrapingPipeline
 
 
 # test crawling
-
 def test_scrape(url_base: str, seed_path: str, num_pages: int, crawl_path: str, scrape_path: str, start: int = 2, seed_limit: int = None, crawl_limit: int = None):
     pipeline = ALZConnectedScrapingPipeline()
 
